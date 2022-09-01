5483a4c1f4743dd260cb5482a696369eedad4a75
Gary Mc Dermott <garymcdermott@google.com>
2022-09-01 12:18:36 -0300

    atlantis scale zero

--- variables.tf
+++ variables.tf
@@ -19,25 +19,25 @@ variable "region" {
 variable "enablek8" {
   description = "Enable GKE"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "enablelinuxk8" {
   description = "Enable GKE Linux node pool"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "enablewink8" {
   description = "Enable GKE Windows node pool"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "enablecloudsql" {
   description = "Enable the CloudSQL instance or not"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "subnet" {
